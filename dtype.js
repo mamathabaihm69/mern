36ccb19921440b7b7d7e35344d8c0fdeda06a0de
mamathabaihm69 <mamathabaihm69@gmail.com>
2025-09-14 18:20:03 +0530

    Add files via upload

---
+++ dtype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+2) we are using node js not browser
+
+console.log(3+
+    3)//code read ability sould be correct
+console.log("mamatha")
+
+let name="mamatha"
+let age =18
+let isLoggedIn=false
+
+//premitive
+//number=> 2 to power 53
+//bigint= large data
+//string sigle or double ' ""
+//boolen=true or false
+//null=standalone value, we can assign in programing
+//undefined= value doen't decalred(assigned)
+//symbol= finds uniqness 
+
+
+//object
+console.log(typeof"mamatha");
+console.log(typeof null);//null is a object,object
+console.log(typeof undefined);//undefined
